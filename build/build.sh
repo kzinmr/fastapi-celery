d43128402eea523f9dd683c446eaba8b9288eb0e
Kazuki Inamura <kzinmr109@gmail.com>
2024-10-16 02:53:43 +0900

    fixme

---
+++ build/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -exu
+platform=${1:-linux/arm64}
+
+ENV="${ENV:-development}"
+
+DOCKER_REGISTRY=${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com
+
+RELEASE="${RELEASE:-false}"
+RELEASE_OPT=""
+if [ "${RELEASE}" == "true" ]; then
+  RELEASE_OPT="--push"
+fi
+
+DOCKER_FILE_PATH="${DIR}/Dockerfile"
+# frontend と backend の docker build context は異なる
+if [ "${NAME}" == "frontend" ] ; then
+  cd "${DIR}"
+  DOCKER_FILE_PATH="Dockerfile"
+fi
+
+docker buildx build ${RELEASE_OPT} \
+  --platform="${platform}" \
+  --progress=plain \
+  --tag "${DOCKER_REGISTRY}"/"${ENV}"/ai-review/"${NAME}":"${IMAGE_TAG}" \
+  -f "${DOCKER_FILE_PATH}" \
+  .
